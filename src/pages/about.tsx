c22352f8b80042118d437e0b19fbe209b9fba939
Nuu-maan <n82238895@gmail.com>
2024-10-15 11:37:24 +0530

    sdsdhgfaouwe

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+// src/pages/about.tsx
+const About = () => {
+    return (
+      <div>
+        <h1>About Us</h1>
+        <p>Welcome to the about page of this portfolio.</p>
+      </div>
+    );
+  };
+  
+  export default About;
+  
